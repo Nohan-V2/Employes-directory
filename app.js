7f48979147fec69289619109f559140d448ea3d5
Nohan-V2 <nseck@edenschool.fr>
2025-03-06 13:25:39 +0100

    update

--- app.js
+++ app.js
@@ -1,67 +1,60 @@
 const $employeesWrapper = document.querySelector(".employees-wrapper");
+const $employeesDetails = document.querySelector(".employees-details");
 
 console.log($employeesWrapper);
 
-const employeData = {
-  firstName: "data.name.first",
-  lastName: "data.name.last",
-  email: "john@gmail.com",
-  picture: "./user.jpg",
-  city: "Pantin",
-};
+let employees;
 
-fetch("https://randomuser.me/api/?results=12&inc=name,email,picture,location")
+fetch(
+  "https://randomuser.me/api/?results=12&inc=name,email,picture,location,id&nat=FR"
+)
+  .then(function (res) {
+    return res.json();
+  })
   .then(function (data) {
     console.log(data);
-    const firstName = data.name.first;
-    console.log(firstName);
+
+    employees = data.results;
+
+    console.log(employees);
+
+    employees.forEach((element) => {
+      console.log(element);
+
+      const $employeCard = document.createElement("div");
+      $employeCard.classList.add("employe-card");
+      $employeesWrapper.appendChild($employeCard);
+
+      const $picture = document.createElement("img");
+      $picture.classList.add("picture");
+      $picture.src = element.picture.large;
+      $employeCard.appendChild($picture);
+
+      const $textCard = document.createElement("ul");
+      $textCard.classList.add("text-card");
+      $employeCard.appendChild($textCard);
+
+      const $fullName = document.createElement("li");
+      $fullName.classList.add("full-name");
+      $textCard.appendChild($fullName);
+      $fullName.textContent = `${element.name.first} ${element.name.last}`;
+
+      const $email = document.createElement("li");
+      $email.classList.add("email");
+      $textCard.appendChild($email);
+      $email.textContent = element.email;
+
+      const $city = document.createElement("li");
+      $city.classList.add("city");
+      $textCard.appendChild($city);
+      $city.textContent = element.location.city;
+
+      $employeCard.addEventListener("click", function (e) {
+        $employeesWrapper.classList.add("hidden");
+        $employeesDetails.classList.remove("hidden");
+      });
+    });
   })
   .catch(function (err) {
     console.log("Error: " + err);
   });
-
-function tableObjectAPI(data) {
-  let employees = [];
-
-  for (let i = 0; i < 12; i++) {
-    employees.push(data);
-  }
-
-  return employees;
-}
-
-console.log(tableObjectAPI(employeData));
-
-document.addEventListener("DOMContentLoaded", () => {
-  employeesData.forEach((element) => {
-    console.log(element);
-
-    const $employeCard = document.createElement("div");
-    $employeCard.classList.add("employe-card");
-    $employeesWrapper.appendChild($employeCard);
-
-    const $picture = document.createElement("img");
-    $picture.classList.add("picture");
-    $picture.src = element.picture;
-    $employeCard.appendChild($picture);
-
-    const $textCard = document.createElement("ul");
-    $textCard.classList.add("text-card");
-    $employeCard.appendChild($textCard);
-
-    const $fullName = document.createElement("li");
-    $fullName.classList.add("full-name");
-    $textCard.appendChild($fullName);
-    $fullName.textContent = `${element.firstName} ${element.lastName}`;
-
-    const $email = document.createElement("li");
-    $email.classList.add("email");
-    $textCard.appendChild($email);
-    $email.textContent = element.email;
-
-    const $city = document.createElement("li");
-    $city.classList.add("city");
-    $textCard.appendChild($city);
-    $city.textContent = element.city;
-  });
-});
